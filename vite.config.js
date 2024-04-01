85364919ff2bb56ab54d81fb784de4afe32b8810
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-04-01 04:29:33 +0200

    proxy update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://typemonkey-app.vercel.app",
+        target: "https://miguel-main-server.vercel.app",
         changeOrigin: true,
       },
     },
