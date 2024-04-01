9b3a88fcb31d2d6e7a30a160cb344ef069641b34
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-04-01 04:19:22 +0200

    added hook game end

---
+++ src/hooks/useGameEnd.js
@@ -0,0 +1,42 @@
+import { useEffect, useMemo } from "react";
+import { useWordsStore } from "../store/useWords";
+import { GAME_MODE } from "../utils/constants";
+
+const useGameEnd = () => {
+  const {
+    words,
+    wordIndex,
+    letterIndex,
+    setAppStateRunning,
+    gameMode,
+    setAppStateFinished,
+    timeRemaining,
+    calculateResults,
+  } = useWordsStore();
+
+  const hasFinished = useMemo(() => {
+    if (gameMode === GAME_MODE.WORDS || gameMode === GAME_MODE.QUOTE) {
+      //Devolvemos true si el letterIndex y el WordIndex son el ultimo de la lista de palabras (si hemos llegado al final)
+      return (
+        wordIndex === words.length - 1 &&
+        letterIndex === words[wordIndex].length
+      );
+    }
+
+    // If the game mode is time, we finish the game when the time runs out
+    if (gameMode === GAME_MODE.TIME) {
+      return timeRemaining === 0;
+    }
+
+    return false;
+  }, [wordIndex, letterIndex, gameMode, words, timeRemaining]);
+
+  useEffect(() => {
+    if (hasFinished) {
+      calculateResults();
+      setAppStateFinished();
+    }
+  }, [hasFinished, setAppStateRunning, setAppStateFinished, calculateResults]);
+};
+
+export default useGameEnd;
